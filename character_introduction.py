b86eab37b6740d0396ef242a758161600143302c
Superjoaq978 <joaquin.rosales@ucas-edu.net>
2026-08-20 09:53:22 -0600

    this is code

---
+++ character_introduction.py
@@ -0,0 +1,8 @@
+# Joaquin Rosales, Character Introduction, Programming 1
+
+character_name = "Mickey Mouse"
+character_age = "97"
+character_job = "Steamboat Captain"
+character_home = "Toontown"
+
+print("Hot Diggitiy Dog, I'm " + character_name + " I am " + character_age + " years old. Let's gear up for some fun! I, the " + character_job + " will escort you to my home of " + character_home + " Oh boy!")
