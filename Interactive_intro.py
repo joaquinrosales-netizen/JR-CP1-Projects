923c5c0fe7a269d15fff36e196dd2b2a25512511
Superjoaq978 <joaquin.rosales@ucas-edu.net>
2026-08-26 09:45:20 -0600

    This is the interactive introduction assignment

---
+++ Interactive_intro.py
@@ -0,0 +1,15 @@
+print("Welcome to the program user")
+name = input("Whats your name user")
+print("Ok, " + name + " I hope you are doing well.")
+food = input("What is your favorite food")
+print("Nice, I can't eat " + food + " Because I am a program, but any human can!")
+superhero = input("Who is your favorite superhero")
+if superhero == "Spider-man":
+	print("Hey, Spideys my favorite too!")
+else:
+	print("Nice, that is a cool superhero!")
+age = int(input("Finnaly, how old are you?"))
+if age < 18:
+	print("Bro's childhood is cocomelon")
+if age > 18:
+	print("Did you know the stegasorous, you ancient bloke? Bro went to class with cleopatra lol")
