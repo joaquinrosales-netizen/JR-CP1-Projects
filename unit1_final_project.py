12640357e75b2febe1039ebb9b615dd73d99960d
Superjoaq978 <joaquin.rosales@ucas-edu.net>
2026-08-27 09:15:23 -0600

    New code

--- unit1_final_project.py
+++ unit1_final_project.py
@@ -1,3 +1,4 @@
+#Unit 1 Final Joaquin Rosales
 print("Welcome to the program user")
 name = input("Whats your name user")
 print("Ok, " + name + " I hope you are doing well.")
@@ -13,3 +14,12 @@ if age < 18:
 	print("Bro's childhood is cocomelon")
 if age > 18:
 	print("Did you know the stegasorous, you ancient bloke? Bro went to class with cleopatra lol")
+print("Anyway, " + name + " I have a job for you")
+print("I want you to get me the best " + food + " ever")
+quest=input("Do you accept " + str(name) + " ?")
+if quest == "no":
+	print("Darn it, go away if your too lazy then!")
+if quest == "yes":
+	print("Mmm, thanks for the meal!")
+if quest == "Aren't you a program?":
+	print("Oh yeah my bad")
