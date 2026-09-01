ec44e47b4c514f2c1021117a18f0a630d90798b4
superjoaq978 <joaquin.rosales@ucas-edu.net>
2026-09-01 09:52:28 -0600

    Average grade

---
+++ average_grade.py
@@ -0,0 +1,11 @@
+# Joaquin Rosales Programming 1 Grade Average Assignment
+
+print("How many classes do you have and what are they?")
+periodone = input("What is your first period?")
+periodtwo = input("What is your second period?")
+periodthree = input("What is your third period?")
+periodfour = input("Do you have lunch for fourth period or advisory?")
+if periodfour == "lunch":
+    print("Ok, so you have advisory for fifth period")
+if periodfour == "advisory":
+    print("Ok, so you have lunch for fifth period")
