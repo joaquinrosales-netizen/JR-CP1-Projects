4617c6dc9ab6002d664cea4ab6dc056f17a8c93d
superjoaq978 <joaquin.rosales@ucas-edu.net>
2026-09-02 09:52:00 -0600

    note and average grade assingment

--- average_grade.py
+++ average_grade.py
@@ -9,3 +9,24 @@ if periodfour == "lunch":
     print("Ok, so you have advisory for fifth period")
 if periodfour == "advisory":
     print("Ok, so you have lunch for fifth period")
+periodsix = input("What is your sixth period?")
+periodseven = input("What is your seventh period?")
+periodeight = input("What is your last period?")
+print("Ok I am now going to ask you your grade in each of your classes")
+onegrade = input("What is your grade in " + periodone + "?")
+twograde = input("What is your grade in " + periodtwo + "?")
+threegrade = input("What is your grade in " + periodthree + "?")
+if periodfour == "lunch":
+    print("I won't grade you for this")
+if periodfour == "advisory":
+    print("I will now ask you your grade in advisory, if you have a grade. If none, just type 0.")
+fivegrade = input("What is your grade in advisory?")
+sixgrade = input("What is your grade in " + periodsix + "?")
+if sixgrade == "none":
+    print("Ok, it's because it's seminary")
+else:
+    print("Ok")
+sevengrade = input("What is your grade in" + periodseven + "?")
+eightgrade = input("What is your grade in" + periodeight + "?")
+classes = input("I will ask you, how many classes do you have that have grades?")
+
---
+++ notething.py
@@ -0,0 +1,29 @@
+# string notes or something
+
+# strings are a collection of characters held together by quotation marks
+
+name = "Joaquin"
+
+age = "15"
+
+print(age + "2")
+
+print(name+" "+age)
+
+first_name = "Joaquin"
+last_name = "Rosales"
+full_name = first_name + " " + last_name
+print(full_name)
+
+sentence = 'then he said \n"That isn\'t fair"'
+print(sentence)
+
+print(full_name * 4)
+sentence = "The quick brown fox jumps over the lazy dog."
+print(sentence)
+print(sentence.find("w"))
+print(sentence[9:13])
+word = input("What word do you want?")
+start = sentence.find(word)
+length = len(word)
+print(sentence[start:start+length])
